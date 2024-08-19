db93f1b851c125b42657252eaae40772e879aa78
Laiba Riaz <106284661+Laibii1@users.noreply.github.com>
2024-08-19 15:30:01 +0500

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,7 @@
 import { google } from "@ai-sdk/google";
 import { type CoreMessage, streamText } from "ai";
-
 export const preferredRegion = 'fra1';
-export const maxDuration = 300;
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages }: { messages: CoreMessage[] } = await req.json();
